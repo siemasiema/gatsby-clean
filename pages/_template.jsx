37dcc23b37f98c22820749abbbf2caa32806ee06
siemasiema <q1029384756@wp.pl>
2018-03-02 22:58:59 +0100

    q

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -38,7 +38,7 @@ module.exports = React.createClass({
                 textDecoration: 'none',
               }}
             >
-              Your Brand!
+              START
             </Link>
           </Container>
         </Headroom>
